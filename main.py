6cfe63d34c94d1a92fce6f5f54cef119b1bf683c
Cheef <kalmykovmitya2005@gmail.com>
2023-09-12 14:21:20 +0300

    feature, fix: добавление функции ввода пользовательской информации для регистрации, исправление не пропадающей кнопки под профилем после регистрации

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ check_registration = False
 bot = telebot.TeleBot(token)
 @bot.message_handler(commands=['start'])
 def start(message):
+    global check_registration
     info_registration = 'Вы зарегистрированны'
     keyboard = types.ReplyKeyboardMarkup()
     couples = types.KeyboardButton('Рассписание пар')
@@ -21,30 +22,45 @@ def start(message):
     keyboard.row(time, information)
     keyboard.row(profile)
     
-    if registration.chack_registration(message.from_user.id) == True:
+    if registration.chack_registration(message.from_user.id):
         check_registration = True
-        if not check_registration:
-            info_registration = 'Вы не зарегистрированны'
+    if check_registration == False:
+        info_registration = 'Вы не зарегистрированны'
      
     bot.send_message(message.chat.id, f'Добрый день, {message.from_user.first_name}. {info_registration}', reply_markup=keyboard)
     
+@bot.message_handler(commands=['reg'])
+def registration_in_bot(message):
+    user_input_cache = message.text.split()
+    user_information = {
+        'user_id': message.from_user.id,
+        'user_name': message.from_user.first_name[0],
+        'group_name': user_input_cache[1],
+        'password': user_input_cache[2],
+        'email': user_input_cache[-1],
+    }
+    registration_output = registration.Registration(id=user_information['user_id'], name=user_information['user_name'], name_group=user_information['group_name'], password=user_information['password'], email=user_information['email'])
+    registration_output.registration()
+    bot.send_message(message.chat.id, 'Вы зарегистрированны')
+
 @bot.callback_query_handler(func=lambda call: True)
 def input_user_information(call):
     if call.data == 'registration':
-        bot.send_message(call.message.chat.id, 'Введите данный о себе по примеру: /reg название_и_номер_групп пароль_от_личного_кабинета электронную_почту_на_которую_зерегистрирован_личный_кабинет (данные от личного кабинета не обязательные)')
+        bot.send_message(call.message.chat.id, 'Введите данный о себе по примеру: /reg название_и_номер_групп пароль_от_личного_кабинета электронную_почту_на_которую_зерегистрирован_личный_кабинет (данные от личного кабинета не обязательные, если вы не хотите вводить данные о личном кабинете, то вместо них впешите нули)')
     
 @bot.message_handler(content_types=['text'])
 def profile(message):
     if message.text == 'Профиль':
         message_keyboard = types.InlineKeyboardMarkup()
         registration_button = types.InlineKeyboardButton('Зарегистрироваться', callback_data='registration')
-        message_keyboard.add(registration_button)
         status = 'вы не зарегистрированны'
         user_id = message.from_user.id
         name = message.from_user.first_name
         group_name = '*'
         if registration.chack_registration(user_id):
             status = 'вы зарегистрированны'
+        else:
+            message_keyboard.add(registration_button)
         bot.send_message(message.chat.id, f"""<b>├ID:</b> {user_id}\n<b>├Имя:</b> {name}\n<b>├Группа:</b> {group_name}\n<b>├Статус:</b> {status}""", parse_mode='html', reply_markup=message_keyboard)     
 
 bot.infinity_polling()
