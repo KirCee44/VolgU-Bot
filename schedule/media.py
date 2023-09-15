91af01761df6d1a8744656a24c977c6e98d016b9
Cheef <bananchik2211@mail.ru>
2023-09-15 15:52:17 +0300

    feature: добавление функции генерации расписания

--- schedule/media.py
+++ schedule/media.py
@@ -1,3 +1,4 @@
+shedule_image_save = 'media\Sak\group_212\week_0\image.jpg'
 pairing_schedule = 'media\pairing_schedule.jpg'
-schedule_template = 'media\schedule_template.png'
+schedule_template = 'media\schedule_template.jpg'
 schedule_week = 'media\Sak\group_212\week_0\week.txt'
--- schedule/schedule.py
+++ schedule/schedule.py
@@ -1,4 +1,4 @@
-import media
+from PIL import ImageFont, Image, ImageDraw
 
 week_days = ['Понедельник','Вторник','Среда','Четверг','Пятница','Суббота', 'Воскресенье']
 
@@ -8,4 +8,13 @@ def create_schedule_day(week_day, url):
     day_1 = file_list.index(week_days[week_day]+'\n')
     day_2 = file_list.index(week_days[week_day+1]+'\n')
     
-    return file_list[day_1:day_2]
+    return ''.join(file_list[day_1+1:day_2])
+
+def geniration_schedule_image(url_image, url_save, week_day, url_scedule_text):
+    text = create_schedule_day(week_day, url_scedule_text)
+    image = Image.open(url_image)
+    image_drow = ImageDraw.Draw(image)
+    font = ImageFont.truetype("calibri.ttf", 30, encoding='UTF-8')
+    image_drow.text((90,140), text, font=font, fill="black", align="left")
+    image.save(url_save)
+    return url_save
